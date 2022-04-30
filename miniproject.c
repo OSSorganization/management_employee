d9d63ceaab496d7333b67593e6b6291f49bf3f31
21900162 <21900162@handong.ac.kr>
2022-05-01 01:52:35 +0900

    file수정

--- miniproject.c
+++ miniproject.c
@@ -1,4 +1,4 @@
-#include <stdin.h>
+#include <stdio.h>
 
 typedef struct{
     char name[100];
@@ -9,21 +9,31 @@ typedef struct{
 } Employee;
 
 int create_emp(Employee *a){
-    char [100] name;
-    char [100] date;
+    char name[100] ;
+    char date[100] ;
 
     printf("날짜입력 ex) 7월5일 -> 7/5\n");
     printf("날짜: ");
-    scanf("%d/%d", &month, &day);
+    scanf("%d/%d", &a->month, &a->day);
     printf("이름을 입력하세요: ");
     scanf("%s", a->name);
     printf("시간은 24시 기준 예) 19:15\n");
     printf("\n 출근시간: ");
-    scanf("%d:%d", &in_hour, &in_minute);
+    scanf("%d:%d", &a->in_hour, &a->in_minute);
     printf("퇴근시간: ");
-    scanf("%d:%d", &out_hour, &out_minute);
+    scanf("%d:%d", &a->out_hour, &a->out_minute);
     printf("휴식시간: ");
-    scanf("%d:%d", &rest_hour, &rest_minute);
+    scanf("%d:%d", &a->rest_hour, &a->rest_minute);
 }
 
+int main(){
+    Employee a;
 
+    create_emp(&a);
+    printf("이름: %s\n", a.name);
+    printf("%d월%d일\n", a.month, a.day);
+    printf("출근:%d시%d분\n", a.in_hour, a.in_minute);
+    printf("퇴근:%d시%d분\n", a.out_hour, a.out_minute);
+    printf("휴식:%d시%d분\n", a.rest_hour, a.rest_minute);
+
+}
